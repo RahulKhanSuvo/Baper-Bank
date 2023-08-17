b428c730d8884a43ddace009367adccfff1e586a
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2023-08-17 21:01:21 +0600

    fixed

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,23 +5,29 @@ document
     const withdrawFieldStaring = withdrawField.value;
     const newWithdrawAmount = parseFloat(withdrawFieldStaring);
 
+    withdrawField.value = "";
+
+    if (isNaN(newWithdrawAmount)) {
+      alert("please provide number");
+      return;
+    }
+
     const withdrawTotalElement = document.getElementById("withdraw-total");
 
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
 
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     const ballanceTotalElement = document.getElementById("ballance-total");
     const previousBallanceString = ballanceTotalElement.innerText;
     const previousBallanceTotal = parseFloat(previousBallanceString);
+
     if (newWithdrawAmount > previousBallanceTotal) {
       alert("not enough money");
+      return;
     }
-
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
     const newBallanceTotal = previousBallanceTotal - newWithdrawAmount;
     ballanceTotalElement.innerText = newBallanceTotal;
-
-    withdrawField.value = "";
   });
